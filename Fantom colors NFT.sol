7141781874f3ed65153a2f21981bf9c6473ff57d
ITMB <electrabuzz98@hotmail.com>
2021-09-02 00:49:30 +0200

    Updated with supporting imports and updates to main contract

--- Fantom colors NFT.sol
+++ Fantom colors NFT.sol
@@ -1,19 +1,37 @@
+// Contract based on [https://docs.openzeppelin.com/contracts/3.x/erc721](https://docs.openzeppelin.com/contracts/3.x/erc721)
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-///@dev replace . with github path for easier Remix deployment 
+/*
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+*/
 import "./ERC721.sol";
+import "./Counters.sol";
 import "./Ownable.sol";
+import "./ERC721URIStorage.sol";
 
 
-//Fantom Colors Contract 
-contract fantomColors is ERC721, Ownable {
 
-constructor() {
+contract MyNFT is ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
+    constructor() public ERC721("Fantom Colors", "FTMC") {}
+
+    function mintNFT(address recipient, string memory tokenURI)
+        public payable onlyOwner
+        returns (uint256)
+    {
+        require(balanceOf(msg.sender) == 0, 'Each address may only mint one color');
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(recipient, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
 }
-
-}
-
-
-
