fa02d1d2c9d40a22dc8b51c34d772adb685eee64
ITMB <electrabuzz98@hotmail.com>
2021-09-02 00:23:31 +0200

    First commit - Skeleton
    This is just the skeleton of which we will be building FTM colors

---
+++ Fantom colors NFT.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+///@dev replace . with github path for easier Remix deployment 
+import "./ERC721.sol";
+import "./Ownable.sol";
+
+
+//Fantom Colors Contract 
+contract fantomColors is ERC721, Ownable {
+
+constructor() {
+
+}
+
+}
+
+
+
