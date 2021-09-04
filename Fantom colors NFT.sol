0b2e4d31cb4d260baf7201f1576f5a2f4267381d
ITMB <electrabuzz98@hotmail.com>
2021-09-04 22:27:02 +0200

    Added token transfer

--- Fantom colors NFT.sol
+++ Fantom colors NFT.sol
@@ -12,12 +12,13 @@ import "./ERC721.sol";
 import "./Counters.sol";
 import "./Ownable.sol";
 import "./ERC721URIStorage.sol";
-
+import "./ERC721PresetMinterPauserAutoId.sol";
 
 contract fantomColors is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
+    ///@dev name and shortname can be changed.
     constructor() public ERC721("Fantom Colors", "FTMC") {}
 
     function mintNFT(address recipient, string memory tokenURI)
@@ -28,7 +29,10 @@ contract fantomColors is ERC721URIStorage, Ownable {
         require(balanceOf(msg.sender) == 0, 'Each address may only mint one color');
 
         ///@dev require that the mint costs something, the amount can be changed. 
+        
 
+        ///@dev require that the tokenId is less than number of NFTs to be minted, or else mint will fail. This amount may be changed.
+        require(uint256(_tokenIds) <= 100, 'Sorry, all NFTs has been minted' );
        
        ///@dev this increments the token ID, so that each NFT has a unique ID
         _tokenIds.increment();
