51602a3518f80258efbf569bc2a2f917c92e2d2e
ITMB <electrabuzz98@hotmail.com>
2021-09-05 15:54:25 +0200

    Update

--- Fantom colors NFT.sol
+++ Fantom colors NFT.sol
@@ -13,26 +13,41 @@ import "./Counters.sol";
 import "./Ownable.sol";
 import "./ERC721URIStorage.sol";
 import "./ERC721PresetMinterPauserAutoId.sol";
+import "./IERC20";
 
-contract fantomColors is ERC721URIStorage, Ownable {
+contract fantomColors is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+
+    ///@dev decalre variables
+    address private sender = msg.sender;
+    address private devWallet = "0x3922B6380867f275d5b49Bb8e2868AB2aF34d358";
+    uint256 private nftAmount = 100;
+    bool private hasMintingStarted;
+
+    }
+
 
     ///@dev name and shortname can be changed.
     constructor() public ERC721("Fantom Colors", "FTMC") {}
 
+    ///@dev structure of being able to call mint function is if all require functions are true they are allowed to mint one.
     function mintNFT(address recipient, string memory tokenURI)
-        public payable onlyOwner
+        public payable
         returns (uint256)
     {
-        ///@dev balance of can be changed so that each address may mint more than one $NFT
-        require(balanceOf(msg.sender) == 0, 'Each address may only mint one color');
+        
+        ///@dev require that minting has started
+        require(hasMintingStarted = true, "Minting has not started")
+
+        ///@dev check that we are not minting more NFTs thant there is in existance
+        if(_tokensIds >= nftAmount) {
+            pause();
+        } 
 
         ///@dev require that the mint costs something, the amount can be changed. 
-        
-
-        ///@dev require that the tokenId is less than number of NFTs to be minted, or else mint will fail. This amount may be changed.
-        require(uint256(_tokenIds) <= 100, 'Sorry, all NFTs has been minted' );
+        require(transferFrom(msg.sender, devWallet, 10^7 ether))
+        if(transferFrom() returns true) {
        
        ///@dev this increments the token ID, so that each NFT has a unique ID
         _tokenIds.increment();
@@ -42,5 +57,6 @@ contract fantomColors is ERC721URIStorage, Ownable {
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
+        }
     }
 }
